7f243f34efa805113be59099b8c93e8faf6be73e
Vladimir Berlev <1783633+tegoo@users.noreply.github.com>
2022-04-04 19:58:21 +0200

    Install nvim-lspconfig

--- image/config/nvim/lua/plugins.lua
+++ image/config/nvim/lua/plugins.lua
@@ -1,5 +1,6 @@
 vim.cmd([[
   call plug#begin()
   Plug 'morhetz/gruvbox', { 'commit': 'bf2885a95efdad7bd5e4794dd0213917770d79b7' }
+  Plug 'neovim/nvim-lspconfig', { 'commit': 'ea29110765cb42e842dc8372c793a6173d89b0c4' }
   call plug#end()
 ]])
